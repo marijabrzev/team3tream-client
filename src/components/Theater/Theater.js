ebcd4d43de56d0ff95b9bea3cab1e25d54e1909f
jaydools <134211868+jaydools@users.noreply.github.com>
2023-11-17 11:32:00 -0500

    narrow api results to 4 via slice

--- src/components/Theater/Theater.js
+++ src/components/Theater/Theater.js
@@ -12,7 +12,8 @@ export default function Theater() {
                 const url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
                 const options = {
                     headers: {
-                        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2Q5ZGNhNzg1NGU5YjkyNWU1OThhNWEwNGYxZGU1NCIsInN1YiI6IjY1Mzk2OTUzNjc4MjU5MDEwMTA5Nzg3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WoLIcLWvRhPLmOsxC6_l3eMlU1sHH7c44g9uyAKhXk0",
+                        Authorization:
+                            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjY2Q5ZGNhNzg1NGU5YjkyNWU1OThhNWEwNGYxZGU1NCIsInN1YiI6IjY1Mzk2OTUzNjc4MjU5MDEwMTA5Nzg3YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WoLIcLWvRhPLmOsxC6_l3eMlU1sHH7c44g9uyAKhXk0",
                         accept: "application/json",
                     },
                 };
@@ -20,9 +21,9 @@ export default function Theater() {
                 const response = await axios.get(url, options);
                 let data = response.data;
                 setMovies(data.results);
-                const updatedMovies = data.map(movie => ({
+                const updatedMovies = data.map((movie) => ({
                     ...movie,
-                    posterUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                    posterUrl: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
                 }));
             } catch (error) {
                 setError(error);
@@ -36,14 +37,23 @@ export default function Theater() {
         <div className="theater-container">
             <div className="theater-hero">Hero image</div>
             <div className="theater-text">
-                <h1 className="theater-text__header">choose something to watch before your food gets cold</h1>
-                <h2 className="theater-text__body">quick description on how to use the app. Click on your favourite and watch any, anytime!</h2>
+                <h1 className="theater-text__header">
+                    choose something to watch before your food gets cold
+                </h1>
+                <h2 className="theater-text__body">
+                    quick description on how to use the app. Click on your favourite and watch any,
+                    anytime!
+                </h2>
 
                 <div className="movie-cards-container">
                     {movies ? (
-                        movies.map((movie) => (
+                        movies.slice(0, 4).map((movie) => (
                             <div key={movie.id} className="movie-card">
-                                <img className="movie-card__poster" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                                <img
+                                    className="movie-card__poster"
+                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                                    alt={movie.title}
+                                />
                                 <p className="movie-card__title">{movie.title}</p>
                                 <button className="movie-card__button">Watch Now!</button>
                             </div>
@@ -53,22 +63,6 @@ export default function Theater() {
                     ) : (
                         <p>Loading...</p>
                     )}
-
-                    <div className="movie-card">
-                        <img className="movie-card__poster" src="" alt="movie poster" />
-                        <p>a description of the movie</p>
-                        <button className="movie-card__button">Watch Now!</button>
-                    </div>
-                    <div className="movie-card">
-                        <img className="movie-card__poster" src="" alt="movie poster" />
-                        <p>a description of the movie</p>
-                        <button className="movie-card__button">Watch Now!</button>
-                    </div>
-                    <div className="movie-card">
-                        <img className="movie-card__poster" src="" alt="movie poster" />
-                        <p>a description of the movie</p>
-                        <button className="movie-card__button">Watch Now!</button>
-                    </div>
                 </div>
             </div>
         </div>
