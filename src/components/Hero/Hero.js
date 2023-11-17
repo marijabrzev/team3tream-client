dd4374c3b00c6df690577e946e1d7b8d9ebfdb04
marijabrzev <brzevmarija@gmail.com>
2023-11-17 10:51:43 -0800

    update to styling

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,12 +6,10 @@ function Hero() {
             <section className="hero__container">
                 {/* <div className="hero__container-contentbox"> */}
                     <div className="hero__container-titlebox">
-                        <h1 className="hero__container-title">Title content</h1>
-                        <h1 className="hero__container-title">Title content</h1>
+                        <h1 className="hero__container-title">Find something to watch before your food gets cold.</h1>
                     </div>
                     <div className="hero__container-textbox">
-                        <h2 className="hero__container-text">Subtext will go here.</h2>
-                        <h2 className="hero__container-text">Subtext will go here.</h2>
+                        <h2 className="hero__container-text">Streaming recommendations catered to you through a whole new experience.</h2>
                     </div>
                     {/* </div> */}
                 {/* <img src="" className="hero__container-image" /> */}
