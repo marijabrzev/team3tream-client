16c43883b09b593cca074cdefe4765dc64be4834
marijabrzev <brzevmarija@gmail.com>
2023-11-16 19:30:30 -0800

    update styling to the hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import "./Hero.scss";
+
+function Hero() {
+    return (
+        <>
+            <section className="hero__container">
+                    <div className="hero__container-titlebox">
+                        <h1 className="hero__container-title">Title content</h1>
+                        <h1 className="hero__container-title">Title content</h1>
+                    </div>
+                    <div className="hero__container-textbox">
+                        <h2 className="hero__container-text">Subtext will go here.</h2>
+                        <h2 className="hero__container-text">Subtext will go here.</h2>
+                    </div>
+                {/* <img src="" className="hero__container-image" /> */}
+                <div className="hero__container-image"></div>
+            </section>
+        </>
+    );
+}
+
+export default Hero;
