5227f5c9e14c7d6307bb91e230bfa2e65ca3244d
marijabrzev <brzevmarija@gmail.com>
2023-11-17 10:22:43 -0800

    update to footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,51 +1,70 @@
-import './Footer.scss';
-import twitterIcon from '../../assets/icons/Icon-twitter.svg'
-import facebookIcon from '../../assets/icons/Icon-facebook.svg'
-import instagramIcon from '../../assets/icons/Icon-instagram.svg'
+import "./Footer.scss";
+import twitterIcon from "../../assets/icons/Icon-twitter.svg";
+import facebookIcon from "../../assets/icons/Icon-facebook.svg";
+import instagramIcon from "../../assets/icons/Icon-instagram.svg";
 
 export default function Footer() {
-  return (
-    <div className='footer'>
-      <div className='footer__left'>
-        <div className='footer__left__logo'>
-          logo/name
-        </div>
-        <div className='footer__left__text'>
-          <p>footer text</p>
-          <p>footer text</p>
-          <p>footer text</p>
-        </div>
-        <div className='footer__left__icons'>
-          <a href='https://x.com' target='_blank' rel="noreferrer">
-            <img className='footer__left__icons__icon' src={twitterIcon} alt='twitter icon'/>
-          </a>
-          <a href='https://facebook.com' target='_blank' rel="noreferrer">
-            <img className='footer__left__icons__icon' src={facebookIcon} alt='facebook icon'/>
-          </a>
-          <a href='https://instagram.com' target='_blank' rel="noreferrer">
-            <img className='footer__left__icons__icon' src={instagramIcon} alt='instagram icon'/>
-          </a>
-        </div>
-      </div>
-      <div className='footer__right'>
-        <div className='footer__right__heading'>
-          <p className='footer__right__heading__text'>footer heading</p>
-          <p className='footer__right__heading__text'>footer heading</p>
-        </div>
-        <div className='footer__right__text'>
-          <div>
-            <p className='footer__right__text__content'>footer text</p>
-            <p className='footer__right__text__content'>footer text</p>
-            <p className='footer__right__text__content'>footer text</p>
-          </div>
-          <div>
-            <p>footer text</p>
-            <p>footer text</p>
-            <p>footer text</p>
-          </div>
-        </div>
-      </div>
+    return (
+        <div className="footer">
+            <div className="footer__left">
+                <img src="" className="footer__left__logo" />
+                <div className="footer__left__text">
+                    <p>Immerse yourself in a world of endless entertainment with Kameleon. Let your mood be the guide as you explore a world of content tailored just for you. Elevate your streaming experience with us!</p>
+                </div>
+                <div className="footer__left__icons">
+                    <a href="https://x.com" target="_blank" rel="noreferrer">
+                        <img className="footer__left__icons__icon" src={twitterIcon} alt="twitter icon" />
+                    </a>
+                    <a href="https://facebook.com" target="_blank" rel="noreferrer">
+                        <img className="footer__left__icons__icon" src={facebookIcon} alt="facebook icon" />
+                    </a>
+                    <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                        <img className="footer__left__icons__icon" src={instagramIcon} alt="instagram icon" />
+                    </a>
+                </div>
+            </div>
+            <div className="footer__right">
+                <div className="footer__right__heading">
+                    <p className="footer__right__heading__text">Join our newsletter</p>
 
-    </div>
-  )
+                    <form className="footer__right__heading-form">
+                        <input type="text" placeholder="Enter your email" name="search" className="footer__right__heading-box"></input>
+                        <button type="submit" className="footer__right__heading-button">
+                            Subscribe
+                        </button>
+                    </form>
+                </div>
+                <div className="footer__right__text">
+                    <div className="footer__right-links">
+                      <div className="footer__right-linksleft">
+                        <a href="" className="footer__right__text__contentleft">
+                            Home
+                        </a>
+                        <a href="" className="footer__right__text__contentleft">
+                            About
+                        </a>
+                        <a href="" className="footer__right__text__contentleft">
+                            Blog
+                        </a>
+                        <a href="" className="footer__right__text__contentleft">
+                            Contact Us
+                        </a>
+                        </div>
+                        <div className="footer__right-linksright">
+                        <a href="" className="footer__right__text__contentright">
+                            Privacy
+                        </a>
+                        <a href="" className="footer__right__text__contentright">
+                            Cookies
+                        </a>
+                        <a href="" className="footer__right__text__contentright">
+                            Terms
+                        </a>
+                        <p className="footer__right__text__copyright">Copyright Kameleon Inc., 2023</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
